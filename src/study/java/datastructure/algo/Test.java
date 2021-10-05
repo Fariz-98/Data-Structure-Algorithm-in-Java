4cd70e1d284b2f36963768f73fbc961e434bcc1b
Fariz-01 <farizizham01@gmail.com>
2021-10-05 23:04:18 +0800

    Added leetcode solution

--- src/study/java/datastructure/algo/Test.java
+++ src/study/java/datastructure/algo/Test.java
@@ -6,22 +6,6 @@ public class Test {
 
     public static void main(String[] args) {
 
-        String allowed = "abc";
-        String[] words = {"a","b","c","ab","ac","bc","abc"};
-
-        HashSet<Character> test = new HashSet<>();
-        HashSet<Character> allowedSet = new HashSet<>();
-
-        for (int l = 0; l < allowed.length(); l++) {
-            allowedSet.add(allowed.charAt(l));
-        }
-
-        for (int i = 0; i < words[4].length(); i++) {
-            test.add(words[4].charAt(i));
-        }
-
-        System.out.println(allowedSet.contains(words[4].charAt(1)));
-
     }
 
 }
