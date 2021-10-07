dcbd2d63ff7ec1f6293e3cf5991c588221ef45c3
Fariz Izham <farizizham01@gmail.com>
2021-10-08 01:29:28 +0800

    Added leetcode solution

--- src/study/java/datastructure/algo/Test.java
+++ src/study/java/datastructure/algo/Test.java
@@ -1,13 +1,17 @@
 package study.java.datastructure.algo;
 
 
-import java.util.HashSet;
+import java.util.*;
 import java.util.Iterator;
 
 public class Test {
 
     public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(3,2,1,2,3,4,3,4,5,9,10,11);
 
+        Collections.sort(list);
+
+        System.out.println(list);
     }
 
 }
