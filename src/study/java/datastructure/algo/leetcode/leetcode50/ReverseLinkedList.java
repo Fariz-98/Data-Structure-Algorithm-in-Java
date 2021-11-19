6302dd95bd4cbc4febd06e807de2323efef536a7
Fariz Izham <farizizham01@gmail.com>
2021-11-19 10:31:33 +0800

    Added leetcode solution

---
+++ src/study/java/datastructure/algo/leetcode/leetcode50/ReverseLinkedList.java
@@ -0,0 +1,63 @@
+package study.java.datastructure.algo.leetcode.leetcode50;
+
+// 206
+// Easy
+
+import study.java.datastructure.algo.leetcode.datastructure.ListNode;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode scnd = new ListNode(2);
+        ListNode thrd = new ListNode(3);
+        ListNode frth = new ListNode(4);
+        ListNode ffth = new ListNode(5);
+
+        head.next = scnd;
+        scnd.next = thrd;
+        thrd.next = frth;
+        frth.next = ffth;
+        ffth.next = null;
+
+        ListNode curr = reverseLit(head);
+        while (true) {
+            System.out.println(curr.val);
+            curr = curr.next;
+
+            if (curr == null) {
+                break;
+            }
+        }
+    }
+
+    public static ListNode reverseLit(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode current = head.next;
+        ListNode holder = head.next.next;
+        head.next = null;
+
+        while (true) {
+            current.next = prev;
+
+            if (holder == null) {
+                return current;
+            }
+
+            prev = current;
+            current = holder;
+            holder = current.next;
+
+            if (holder == null) {
+                current.next = prev;
+                break;
+            }
+        }
+        return current;
+    }
+
+}
